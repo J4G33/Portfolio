b498c7d3525af4ee609035225be85aac85dc0f6b
Jorge A. Garcia <127263961+J4G33@users.noreply.github.com>
2023-05-03 13:05:46 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+	document.querySelectorAll('a[href^="#"]').forEach(function(anchor) {
+	  anchor.addEventListener('click', function(event) {
+		event.preventDefault();
+		document.querySelector(this.getAttribute('href')).scrollIntoView({
+		  behavior: 'smooth'
+		});
+	  });
+	});
+  });
+
+  /// Hire me to Email //////
+
+  <script>
+  document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('a[href^="#"]').forEach(function(anchor) {
+      anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+        });
+      });
+    });
+  });
+</script>
+
+
